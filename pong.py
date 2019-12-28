b53c11454dd85e81ea84d41aa1f04628bfc4fec6
Abdurrahman Qureshi <rahman.a.qureshi@gmail.com>
2019-12-28 15:25:33 -0800

    fix memory problem

--- pong.py
+++ pong.py
@@ -23,8 +23,6 @@ class Experience:
         self.reward = reward
         self.done = done
 
-def perceived_brightness(r, g, b):
-   return int(0.299*r + 0.587*g + 0.114*b)
 
 def rgb_frame_to_grayscale(frame):
     """Convert rgb to grayscale. Uses observed luminance to compute grey value.
@@ -34,7 +32,8 @@ def rgb_frame_to_grayscale(frame):
         numpy array 210x160
     """
     rgb = np.array([0.299, 0.587, 0.114])
-    return np.inner(rgb, frame)
+    return np.inner(rgb, frame).astype(np.uint8)
+
 
 def sample_minibatch(experiences, n):
     minibatch = []
